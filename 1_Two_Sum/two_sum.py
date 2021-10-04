7ddde3d29c5f62e72234e246cf2df0149ab9e811
zhd204 <zhd204@gmail.com>
2021-10-04 07:58:01 -0400

    commit

--- 1_Two_Sum/two_sum.py
+++ 1_Two_Sum/two_sum.py
@@ -1,15 +1,26 @@
 from typing import List
 
 
-class Solution:
+class Solution1:
     @staticmethod
     def two_sum(nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
+            for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
 
 
-a = Solution.two_sum([2, 7, 11, 15], 9)
-print(a)
+class Solution2:
+    @staticmethod
+    def two_sum(nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in nums:
+            hashmap[i] = nums.index(i)
 
+        for i in hashmap.keys():
+            if target - i in hashmap:
+                return [hashmap[i], hashmap[target - i]]
+
+
+a2 = Solution2.two_sum([2, 7, 11, 15], 9)
+print(a2)
